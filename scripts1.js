ecfe8081a38591f05dc7b5c203da06d480d6ff67
alkagado <38248590+alkagado@users.noreply.github.com>
2018-05-07 16:33:06 +0200

    Add files via upload

---
+++ scripts1.js
@@ -0,0 +1,24 @@
+$(function(){
+	console.log('DOM loaded - you can have fun');
+});
+
+
+function Button(text) {
+	this.text = text;
+}
+Button.prototype = {
+
+			create: function() {
+				var self = this;
+				this.$element = $('<button>');
+				this.$element.text(this.text);
+				this.$element.click(function() {
+					alert(self.text);
+				});
+				$('body').append(this.$element);
+			}
+}
+ var btn1 = new Button('Hello!');
+  btn1.create();
+  
+ 
